82cd99aa3837130ddfb422a11e0407ce6bdd42c4
nicosposito <nicolas.sposito16@hotmail.com>
2022-08-02 00:23:10 -0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    disableHostCheck: true
+    allowedHosts: "all"
   },
   pluginOptions: {
     vuetify: {
